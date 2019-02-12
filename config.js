b457f7f40746b3a680979eec7ccfe71905f30e46
puya <puyaars@gmail.com>
2019-02-12 12:28:58 +0330

    procfile

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // module.exports.db_url = 'mongodb://localhost:27017/advisor';
 module.exports.db_url = 'mongodb://puyaars:M5HYSCyWuLVtWSn@ds131531.mlab.com:31531/advisor';
-module.exports.port = process.env.port || 5000;
+// module.exports.port = process.env.port || 5000;
+module.exports.port = 447;
